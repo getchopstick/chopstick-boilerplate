e8da94b3780f46902bf0338aa692125e14de9185
Simon Coudeville <simon.coudeville@wijs.be>
2013-03-22 19:24:47 +0100

    added basic mobile nav CSS & js

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,45 @@
+var jsTheme =
+{
+	// init, something like a constructor
+	init: function()
+	{
+		jsTheme.mobileNav.init();
+		console.log("javascript is locked and loaded!") // for testing purposes. Check your console. Delete after you finished reading this. :-)
+	}
 
+}
+
+jsTheme.mobileNav =
+{
+	init: function()
+	{
+		jsTheme.mobileNav.enableMobileNav();
+		jsTheme.mobileNav.buildMobileNav();
+	},
+
+	// CSS is based on the class .mobile-nav
+	//
+	enableMobileNav: function()
+	{
+		$("html").addClass("mobile-nav");
+	},
+
+	// build mobile nav
+	buildMobileNav: function()
+	{
+		var navHolder = $('.header .inner');
+
+		navHolder.prepend('<span class="main-nav-trigger">menu</span>');
+
+		var trigger = $('.main-nav-trigger');
+		var nav = $('.main-nav');
+
+		trigger.on('click', function() {
+			nav.toggle();
+			$(this).toggleClass("trigger-active");
+		});
+	}
+
+}
+
+$(jsTheme.init);
