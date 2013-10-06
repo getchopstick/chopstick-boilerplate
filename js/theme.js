8b430b401affd427abaa50cc0b17c6db62656bc8
Xavier Bertels <github@xavierbertels.com>
2013-10-06 20:58:58 +0200

    Alerts: pimp radius and onclick event

--- js/theme.js
+++ js/theme.js
@@ -47,10 +47,10 @@ jsTheme.forms =
 {
 	init: function()
 	{
-		$('.alert-box .close').on('click', function(e)
+		$('.alert-box').on('click', function(e)
 		{
 			e.preventDefault();
-			$(this).closest('.alert-box').fadeOut(600);
+			$(this).closest('.alert-box').fadeOut(300);
 		});
 	}
 
