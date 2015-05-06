cc5361e0e379e30b3718022621734530595ae489
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2015-05-06 09:53:41 +0200

    Forgotten ; in console.log

--- js/theme.concat.js
+++ js/theme.concat.js
@@ -6,7 +6,7 @@ var jsTheme =
 		jsTheme.mobileNav.init();
 		jsTheme.forms.init();
 		jsTheme.toggle.init();
-		console.log("javascript is locked and loaded!") // for testing purposes. Check your console. Delete after you finished reading this. :-)
+		console.log("javascript is locked and loaded!"); // for testing purposes. Check your console. Delete after you finished reading this. :-)
 	}
 };
 
