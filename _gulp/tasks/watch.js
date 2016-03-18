659c417a4410be24d6967e4b1600d38127e9da85
Xavier Bertels <github@xavierbertels.com>
2016-03-18 12:03:09 +0100

    Run default tasks at least once before watch

--- _gulp/tasks/watch.js
+++ _gulp/tasks/watch.js
@@ -9,7 +9,7 @@ var gulp = require('gulp');
 var config = require('../config');
 
 // task
-gulp.task('watch', ['browsersync'], function () {
+gulp.task('watch', ['default','browsersync'], function () {
     gulp.watch(config.scss.src, ['scss']);
     gulp.watch(config.js.src, ['js--reload']);
     gulp.watch(config.watch.jekyllSource, ['jekyll--rebuild']);
